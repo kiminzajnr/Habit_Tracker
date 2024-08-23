29cca1620d515600201a4d8a76fb6c897564e65f
kiminzajnr <kiminzaeric@gmail.com>
2024-08-23 23:44:08 +0300

    Add display and save habits

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
+habits = ["Test habit", "Test Habit 2"]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Habit Tracker - Home")
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
+    if request.method == "POST":
+        habits.append(request.form.get("habit"))
     return render_template("add_habit.html", title="Habit Tracker - Add Habit")
